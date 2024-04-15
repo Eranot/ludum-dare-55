da0744598bf500ab253504cd00e814b1daeeace5
Renato MK <renato.konflanz@unochapeco.edu.br>
2024-04-14 21:20:06 -0300

    Menu e UI mais ou menos

--- scenes/level/base_level_ui.gd
+++ scenes/level/base_level_ui.gd
@@ -1,6 +1,11 @@
 extends CanvasLayer
 
 @export_file("*.tscn") var options_scene_file
+@onready var menu_button = $HBox/MarginContainer/MenuButton
+
+func _ready():
+	menu_button.pressed.connect(_on_options_button_pressed)
+	
 
 func _on_options_button_pressed():
 	get_tree().paused = true
